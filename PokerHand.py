03954daf8cc503e5908f308276e549a2faac8877
Zviad Kakauridze <zviadd97@gmail.com>
2024-07-18 11:42:30 +0400

    Returning results based on conditions

--- PokerHand.py
+++ PokerHand.py
@@ -37,6 +37,25 @@ def PokerHand():
         if a == 1 or a == 9:
             diff += 1
 
-
+    if flush != 5 and len (pairs) == 2 and len (three) > 0:
+        return "Full House"
+    elif flush != 5 and len (pairs) == 2:
+        return "One Pair"
+    elif flush != 5 and len (pairs) == 4:
+        return "Two Pairs"
+    elif flush != 5 and len (three) > 0:
+        return "Three of a Kind"
+    elif flush != 5 and len (four) > 0:
+        return "Four of a Kind"
+    elif flush != 5 and diff == 4:
+        return "Straight"
+    elif flush == 5 and diff != 4:
+        return "Flush"
+    elif flush == 5 and diff == 4 and royal == 50:
+        return "Royal Flush"
+    elif flush == 5 and diff == 4:
+        return "Straight Flush"
+    else:
+        return "High Card"
 
 print(PokerHand())
